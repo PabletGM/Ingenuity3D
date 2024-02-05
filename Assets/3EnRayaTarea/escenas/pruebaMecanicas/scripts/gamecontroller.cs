8fea3cccbd89e8c5fd7cee1b395b3eb5d2b388a5
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-02-05 03:50:01 +0100

    changes

--- Assets/3EnRayaTarea/escenas/pruebaMecanicas/scripts/gamecontroller.cs
+++ Assets/3EnRayaTarea/escenas/pruebaMecanicas/scripts/gamecontroller.cs
@@ -705,7 +705,7 @@ public class gamecontroller : MonoBehaviour
     {
         //efecto FadeOut
         //board.GetComponent<DOTweenAnimation>().DORestartById("FadeOut");
-        //background.GetComponent<DOTweenAnimation>().DORestartById("FadeOut");
+        background.GetComponent<DOTweenAnimation>().DORestartById("FadeOut");
         //lineas.GetComponentInChildren<DOTweenAnimation>().DORestartById("FadeOut");
         ////TODOS LOS BOTONES
         //Invoke("DesactivarBotones", 0.5f);
