4f43eabe3f97b64fb28ea444a9e49af321bec0f2
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-02-12 02:16:09 +0100

    Minimapa, 7.8, 7.9

--- Assets/Secuencia7/scripts/ChangingDialogues.cs
+++ Assets/Secuencia7/scripts/ChangingDialogues.cs
@@ -24,21 +24,25 @@ public class ChangingDialogues : MonoBehaviour
    private void ChangeTextWithOption()
    {
         //si estamos dentro buscamos saber opcion escogida
-        string opcionElegida = ElegirCaminoMapa.instance.GetChosenOption();
-        //segun la opcion que sea pone un texto u otro
-        switch(opcionElegida)
+        if(ElegirCaminoMapa.instance != null)
         {
-            case "Rio":
-                textToModify.text = "Tu decisión de cruzar el río no ha sido la más adecuada. ";
-                break;
+            string opcionElegida = ElegirCaminoMapa.instance.GetChosenOption();
+            //segun la opcion que sea pone un texto u otro
+            switch (opcionElegida)
+            {
+                case "Rio":
+                    textToModify.text = "Tu decisión de cruzar el río no ha sido la más adecuada. Es la opción más peligrosa y puede que nunca lleguemos a nuestro destino.";
+                    break;
 
-            case "CaminoEstablecido":
-                textToModify.text = "Tu decisión de ir por el camino establecido no ha sido la más acertada.  ";
-                break;
+                case "CaminoEstablecido":
+                    textToModify.text = "Tu decisión de ir por el camino establecido no ha sido la más acertada. Tardaremos mucho más y no podemos permitirnos perder más tiempo. ";
+                    break;
 
-            case "Bosque":
-                textToModify.text = "Tu decisión de atravesar la selva no ha sido la más adecuada. ";
-                break;
+                case "Bosque":
+                    textToModify.text = "Tu decisión de atravesar la selva no ha sido la más adecuada. Es la opción más peligrosa y puede que nunca lleguemos a nuestro destino.";
+                    break;
+            }
         }
+        
     }
 }
