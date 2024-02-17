013acd02051eba96395fd8c96729f308059ee7a4
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-02-17 10:50:44 +0100

    RESOLUCION PANTALLA

--- Assets/Secuencia7/scripts/resolucionPantalla/resolucionPantalla.cs
+++ Assets/Secuencia7/scripts/resolucionPantalla/resolucionPantalla.cs
@@ -31,10 +31,12 @@ public class resolucionPantalla : MonoBehaviour
         if (aspectRatio > 1.7f && aspectRatio < 1.8f)
         {
             aspectRatioCategory = "16:9";
+            item16x9_1920x1080.SetActive(true);
         }
         else if (aspectRatio > 1.5f && aspectRatio < 1.6f)
         {
             aspectRatioCategory = "16:10";
+            item16x10.SetActive(true);
         }
         else
         {
