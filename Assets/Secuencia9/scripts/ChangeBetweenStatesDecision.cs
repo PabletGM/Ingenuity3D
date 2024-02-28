f72a79a320f1d0ff3674ccfccab6ede72efbda51
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-02-28 13:10:46 +0100

    changebetweenDecisions

--- Assets/Secuencia9/scripts/ChangeBetweenStatesDecision.cs
+++ Assets/Secuencia9/scripts/ChangeBetweenStatesDecision.cs
@@ -1,8 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeBetweenStatesDecision : MonoBehaviour
 {
-    
+    [Header("Sprites references")]
+    [SerializeField]
+    private Image spriterenderer;
+    [SerializeField]
+    private Sprite firstSprite;
+    [SerializeField]
+    private Sprite secondSprite;
+    [SerializeField]
+    private Sprite thirdSprite;
+
+    [Header("timeBetweenImageChange")]
+    [SerializeField]
+    private float timeFirstToSecond;
+    [SerializeField]
+    private float timeSecondToThird;
+    [SerializeField]
+    private float timeThirdToFirst;
+
+
+    private void Start()
+    {
+        Invoke("ChangeFromFirstToSecond", timeFirstToSecond);
+    }
+
+    private void ChangeFromFirstToSecond()
+    {
+        spriterenderer.sprite = secondSprite;
+        Invoke("ChangeFromSecondToThird", timeFirstToSecond);
+    }
+
+    private void ChangeFromSecondToThird()
+    {
+        spriterenderer.sprite = thirdSprite;
+        Invoke("ChangeFromThirdToFirst", timeSecondToThird);
+    }
+
+    private void ChangeFromThirdToFirst()
+    {
+        spriterenderer.sprite = firstSprite;
+        Invoke("ChangeFromFirstToSecond", timeThirdToFirst);
+    }
 }
