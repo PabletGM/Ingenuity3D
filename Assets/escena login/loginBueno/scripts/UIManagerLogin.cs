03e63cacbe56ff9f0388cfa826d6f9b9b75d2e1e
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-04-16 00:56:56 +0200

    5.1 y 5.2

--- Assets/escena login/loginBueno/scripts/UIManagerLogin.cs
+++ Assets/escena login/loginBueno/scripts/UIManagerLogin.cs
@@ -545,7 +545,7 @@ public class UIManagerLogin : MonoBehaviour
             else
             {
                 //SceneManager.LoadScene("escenaIntro");
-                 SceneManager.LoadScene("4.1TareasReparacionDialogo");
+                 SceneManager.LoadScene("5.1ReunionComandante");
             }
         }
 
