056aae08f34e351fceebee9b8c4590e575c08180
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-06-23 21:59:47 +0200

    git lfs prueba

---
+++ Assets/intro/FinishIntro.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FinishIntro : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private GameObject nextScene;
+
+    void Start()
+    {
+        
+        // Asegurarse de que el videoPlayer no sea nulo
+        if (videoPlayer != null)
+        {
+            // Suscribir al evento loopPointReached, que se llama cuando el vídeo termina
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        nextScene.GetComponent<NextSceneGenericMethod2>().NextScene();
+    }
+}
